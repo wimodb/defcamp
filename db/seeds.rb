ca99e11b4f88abf8cad257d6ab4504fd6a02038d
wim vermeersch <wimodbvermeersch@gmail.com>
2017-02-12 14:33:41 +0000

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Distillery you probably haven't heard of them crucifix yr 90's. Gluten-free austin everyday carry, vape man braid vegan cornhole photo booth organic celiac mixtape vexillologist. Vegan crucifix fam synth, kickstarter seitan cornhole deep v celiac neutra hexagon glossier kale chips jean shorts truffaut. Seitan blog snackwave umami, beard PBR&B deep v church-key lomo enamel pin vape pork belly sriracha godard. Irony vexillologist prism coloring book tumblr food truck. Etsy ramps subway tile chia swag pop-up. DIY snackwave raclette, migas yr meh ugh actually."
+    )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+    )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "My great service",
+    body: "Green juice hell of semiotics vegan. Waistcoat etsy flannel you probably haven't heard of them raclette hella. Etsy paleo vice, vaporware chicharrones heirloom humblebrag hell of vexillologist. Plaid activated charcoal chartreuse migas biodiesel you probably haven't heard of them, semiotics humblebrag roof party bitters affogato.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+    )
+end
+
+puts "9 portfolio items created"
