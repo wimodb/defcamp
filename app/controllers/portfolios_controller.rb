72d7474c1b6bf4bc8607e76781455aee9a8e64af
wim vermeersch <wimodbvermeersch@gmail.com>
2017-02-14 19:25:18 +0000

    Implemented custom scopes for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,11 @@
 class PortfoliosController < ApplicationController
   
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.ruby_on_rails_portfolio_items
+  end
+  
+  def angular
+    @angular_portfolio_items= Portfolio.angular
   end
   
   def new
