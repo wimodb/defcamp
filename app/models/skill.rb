ca85aefe5f17f7ea85ffd982ad8f0e8a9290d13e
wim vermeersch <wimodbvermeersch@gmail.com>
2017-02-15 16:11:02 +0000

    implemented concern for managing image data

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,5 +1,14 @@
 class Skill < ApplicationRecord
   
+  includes Placeholder
+  
   validates_presence_of :title, :percent_utilized
+  
+   after_initialize :set_defaults
+  
+  def set_defaults
+    #Placeholder is set in models/concerns/placeholder.rb
+    self.badge ||= Placeholder.image_generator(height:'250', width:'250')
+  end
   
 end
