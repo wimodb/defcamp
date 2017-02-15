a3d20c3984aa353c882791067c43808dc7c2a9e5
wim vermeersch <wimodbvermeersch@gmail.com>
2017-02-15 15:55:15 +0000

    add setting-defaults for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,8 @@
 class PortfoliosController < ApplicationController
   
   def index
-    @portfolio_items = Portfolio.ruby_on_rails_portfolio_items
+    #@portfolio_items = Portfolio.ruby_on_rails_portfolio_items
+    @portfolio_items= Portfolio.all
   end
   
   def angular
