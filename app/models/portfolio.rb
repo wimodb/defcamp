66d8adb9fde3fb7ad2147b36c2b7b4ec946421a5
wim vermeersch <wimodbvermeersch@gmail.com>
2017-02-15 16:25:05 +0000

    Integrated technologies model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,7 @@
 class Portfolio < ApplicationRecord
   
+  has_many :technologies
+  
   validates_presence_of :title, :body, :main_image, :thumb_image
   
   def self.angular
@@ -12,6 +14,7 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
   
   def set_defaults
+    #Placeholder is set in models/concerns/placeholder.rb
     self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
     self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
