78597c1a09371609d1708b08654dfb5946412844
wim vermeersch <wimodbvermeersch@gmail.com>
2017-02-18 15:42:27 +0000

    completed authentication with vertial attrs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  validates_presence_of :name
+  
+  def first_name
+    self.name.split.first
+  end
+  
+  def last_name
+    self.name.split.last
+  end
+  
+  def first_letter
+    self.name.split('').first.capitalize 
+  end
+  
 end
