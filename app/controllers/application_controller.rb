916a49cec57b4fa6acbea115f332c166fbd0f172
wim vermeersch <wimodbvermeersch@gmail.com>
2017-02-20 20:12:27 +0000

    Added guest user for current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  #Needed for custom 'name'-field for authentication.
   include DeviseWhitelist
+  
+  #Needed to attach a custom param to path to see where user comes from.
   include SetSource
   
+  #'null-object-pattern' Needed to create kind of guest user so there is always a current_user 
+  include CurrentUserConcern
 
 end
