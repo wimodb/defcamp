25414a66c046eea7ca64c10d60e6f35e8dfe223f
wim vermeersch <wimodbvermeersch@gmail.com>
2017-02-23 19:18:53 +0000

    implemented content tag helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,10 @@ module ApplicationHelper
     end
   end
   
+  def source_helper
+    if session[:source]
+      content_tag(:p, "Thanks for visiting me from #{session[:source]}", class: "source-greeting")
+    end
+  end
+  
 end
