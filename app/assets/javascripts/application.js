c6325f61509e73ac7854be9fccf26bf838082986
wim vermeersch <wimodbvermeersch@gmail.com>
2017-03-11 09:22:52 +0000

    installed jquery ui sortable html5 library and css calls

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
 //= require bootstrap-sprockets
+//= require jquery_ujs
+//= require jquery-ui
+//= require html.sortable
 //= require turbolinks
 //= require_tree .
